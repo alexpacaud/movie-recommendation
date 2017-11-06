f52addea1329004ff160b4e83d09a2c66630cc6e
Alexandre Pacaud <alexandre.pacaud@eleves.enpc.fr>
2017-11-06 19:38:14 +0100

    Add feature Display Movie ideas

--- server/app.py
+++ server/app.py
@@ -1,3 +1,8 @@
+
+import csv
+import pandas as pd
+
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -7,4 +12,7 @@ def hello_world():
 
 @app.route('/my-movies')
 def list_user_movies():
-    return render_template('my-movies.html', page_title='My Movies')
+    my_movies_rated = pd.read_csv('./data/my-ratings.csv', sep=',', header=0)
+    all_movies=pd.read_csv('./data/movies.csv', sep=',',header=0)
+    my_movies_rated = pd.merge(my_movies_rated, all_movies, on='movieId', how='inner')
+    return render_template('my-movies.html', page_title='My Movies', my_movies_rated=my_movies_rated)
